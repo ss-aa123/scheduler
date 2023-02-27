7ef75983552f7bfa88efe77235784ab22c4a1020
ss-aa123 <neermas1@hotmail.com>
2023-02-27 12:25:08 -0500

    apply styling on DayListItem component based on current state of element

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,8 +4,8 @@ import "components/Button.scss";
 
 export default function Button(props) {
   const buttonClass = classNames("button", {
-    " button--confirm": props.confirm,
-    " button--danger": props.danger,
+    "button--confirm": props.confirm,
+    "button--danger": props.danger,
   });
 
   return (
