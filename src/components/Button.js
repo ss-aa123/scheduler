17244edc475bdbf7b572833e629b0dd0eccf9ccb
ss-aa123 <neermas1@hotmail.com>
2023-02-27 19:15:24 -0500

    create DayList component; serves as container for DayListItem components

--- src/components/Button.js
+++ src/components/Button.js
@@ -19,16 +19,3 @@ export default function Button(props) {
   );
 }
 
-/*
-DayListItem props
-   - name:String the name of the day
-   - spots:Number the number of spots remaining
-   - selected:Boolean true or false declaring that this day is selected
-   - setDay:Function accepts the name of the day eg. "Monday", "Tuesday"
-
-DayList props
-   - days:Array a list of day objects (each object includes an id, name, and spots)
-   - day:String the currently selected day
-   - setDay:Function accepts the name of the day eg. "Monday", "Tuesday"
-
-*/
---
+++ src/components/DayList.js
@@ -0,0 +1,26 @@
+import React from "react";
+import DayListItem from "./DayListItem";
+
+export default function DayList(props) {
+  const days = props.days.map((day) => {
+    return (
+      <DayListItem
+        key={day.id}
+        name={day.name}
+        spots={day.spots}
+        selected={day.name === props.day}
+        setDay={props.setDay}
+      />
+    );
+  });
+
+  return <ul>{days}</ul>;
+}
+
+/*
+DayList props
+   - days:Array a list of day objects (each object includes an id, name, and spots)
+   - day:String the currently selected day
+   - setDay:Function accepts the name of the day eg. "Monday", "Tuesday"
+
+*/
