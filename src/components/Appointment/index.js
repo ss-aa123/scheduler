e420b6ab787a6cd0b3f703c29908a4925e3796a4
ss-aa123 <neermas1@hotmail.com>
2023-02-28 21:26:01 -0500

    build Appointment components children and their stories

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -11,13 +11,11 @@ export default function Appointment(props) {
 
 
 /*
-children: 
-index
-Empty
-Show
-Form
-Confirm
-Status
-Error
-
+Appointment component's children:
+  - Header displays the time for the appointment
+  - Empty allows a user to choose which time slot to book
+  - Show allows a user to see an existing appointment
+  - Confirm allows a user to confirm a destructive action
+  - Status informs the user that an operation is in progress
+  - Error informs the user when an error occurs
 */
