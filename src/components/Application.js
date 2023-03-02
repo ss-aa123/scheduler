6b727a9af662aff1b6a5066cd8ab6562fce6a473
ss-aa123 <neermas1@hotmail.com>
2023-03-01 21:14:23 -0500

    add useEffect and axios promise to select days

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
@@ -41,26 +42,18 @@ const appointments = {
     time: "4pm",
   },
 };
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    const testURL = `/api/days`;
+    axios.get(testURL).then((response) => {
+      console.log(response.data);
+      setDays([...response.data]);
+    });
+  }, []);
 
   return (
     <main className="layout">
